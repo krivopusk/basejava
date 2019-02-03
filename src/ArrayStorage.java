9a66633f6e68e86f6aca598dad861e4bbfc8b156
krivopusk <annkrivopusk@gmail.com>
2019-02-03 10:53:58 +0300

    HW1: Реализация класса ArrayStorage, определение методов save, get, delete, size, clear, getAll

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,57 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    private int realSize = 0;
 
     void clear() {
+        for (int i = 0; i < size(); i++) {
+            storage[i] = null;
+        }
+        realSize = 0;
     }
 
     void save(Resume r) {
+        if (size() == 10000) {
+            System.out.println("ArrayStorage is full. You can not add a new resume");
+            return;
+        }
+        storage[size()] = r;
+        realSize++;
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < size(); i++) {
+            if ((storage[i].uuid).equals(uuid)) {
+                return storage[i];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < size(); i++) {
+            if (uuid.equals(storage[i].uuid)) {
+                //the element at the end of the array replaces the element which we want to remove
+                storage[i] = storage[size() - 1];
+                storage[size() - 1] = null;
+                break;
+            }
+        }
+        realSize--;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOfRange(storage, 0, size());
     }
 
     int size() {
-        return 0;
+        return realSize;
     }
 }
