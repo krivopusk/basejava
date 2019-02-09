7b09e23cb5d36d6133bfb5bc4a93063023b56fca
krivopusk <annkrivopusk@gmail.com>
2019-02-09 13:11:41 +0300

    HW4: Reflection

--- src/com/resumebase/webapp/MainReflection.java
+++ src/com/resumebase/webapp/MainReflection.java
@@ -3,20 +3,27 @@ package com.resumebase.webapp;
 import com.resumebase.webapp.model.Resume;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.*;
+import java.lang.reflect.Method;
 
 public class MainReflection {
 
-    public static void main(String[] args) throws IllegalAccessException {
+    public static void main(String[] args) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
         Resume r = new Resume();
-        Field field = r.getClass().getDeclaredFields()[0];
+        Class<? extends Resume> resumeClass = r.getClass();
+        Field field = resumeClass.getDeclaredFields()[0];
         field.setAccessible(true);
         System.out.println(field.getName());
         field.get(r);
         System.out.println(r);
         field.set(r, "new_uuid");
+        System.out.println(r);
 
         //TODO invoke r.toString() via Reflection
-        System.out.println(r);
+        Method method = resumeClass.getMethod("toString");
+        Object result = method.invoke(r);
+        System.out.println(result);
 
 
     }
