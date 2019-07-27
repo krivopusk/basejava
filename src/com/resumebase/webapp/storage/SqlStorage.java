3269efdd9530d50fe9297bd4990679bfc02a622e
krivopusk <annkrivopusk@gmail.com>
2019-07-27 14:12:33 +0300

    lesson14: JOIN, LEFT JOIN with examples

--- src/com/resumebase/webapp/storage/SqlStorage.java
+++ src/com/resumebase/webapp/storage/SqlStorage.java
@@ -1,6 +1,7 @@
 package com.resumebase.webapp.storage;
 
 import com.resumebase.webapp.exception.NotExistStorageException;
+import com.resumebase.webapp.model.ContactType;
 import com.resumebase.webapp.model.Resume;
 import com.resumebase.webapp.sql.SqlHelper;
 
@@ -8,6 +9,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class SqlStorage implements Storage {
     public final SqlHelper sqlHelper;
@@ -23,13 +25,22 @@ public class SqlStorage implements Storage {
 
     @Override
     public Resume get(String uuid) {
-        return sqlHelper.execute("SELECT * FROM resume r WHERE r.uuid =?", ps -> {
+        return sqlHelper.execute("SELECT * FROM resume r " +
+                                 " LEFT JOIN contact c " +
+                                 " ON r.uuid = c.resume_uuid " +
+                                 " WHERE r.uuid =?", ps -> {
             ps.setString(1, uuid);
             ResultSet rs = ps.executeQuery();
             if (!rs.next()) {
                 throw new NotExistStorageException(uuid);
             }
-            return new Resume(uuid, rs.getString("full_name"));
+            Resume resume = new Resume(uuid, rs.getString("full_name"));
+            do {
+                String value = rs.getString("value");
+                ContactType type = ContactType.valueOf(rs.getString("type"));
+                resume.addContact(type, value);
+            } while (rs.next());
+            return resume;
         });
     }
 
@@ -53,6 +64,16 @@ public class SqlStorage implements Storage {
             ps.execute();
             return null;
         });
+
+        for (Map.Entry<ContactType, String> e : r.getContacts().entrySet()) {
+            sqlHelper.<Void>execute("INSERT INTO contact (resume_uuid, type, value) VALUES (?,?,?)", ps -> {
+                ps.setString(1, r.getUuid());
+                ps.setString(2, e.getKey().name());
+                ps.setString(3, e.getValue());
+                ps.execute();
+                return null;
+            });
+        }
     }
 
     @Override
